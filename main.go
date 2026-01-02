054155ba6c447335db3471d0286a3220b0783c0c
Daniel Hannon <hello111f@gmail.com>
2026-01-02 13:09:33 +0000

    Some minor changes

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import(
+import (
 	"github.com/google/uuid"
 
 	"os"
 	"os/exec"
-	
+
 	"time"
 
 	"fmt"
@@ -18,7 +18,7 @@ type FileFiller struct {
 	FileName     string
 	FileNameCaps string
 	AuthorName   string
-	UUID		 string
+	UUID         string
 	Year         int
 }
 
@@ -26,39 +26,39 @@ func CreateFileFiller(fName string) FileFiller {
 
 	// Remove illegal characters from the filenames
 	re := regexp.MustCompile(`[\\ -.\/:*?\"<>|$?{}\[\]]`)
-	fName = re.ReplaceAllString(fName,"")
+	fName = re.ReplaceAllString(fName, "")
 	fName = strings.ToLower(fName)
 	fNameUpper := strings.ToUpper(fName)
-	
+
 	// get the git username for generating the name
-	command := exec.Command("git","config","user.name")
+	command := exec.Command("git", "config", "user.name")
 	authNameTmp, err := command.Output()
 	authName := ""
 	if err == nil {
 		authName = string(authNameTmp)
 	}
-	authName = strings.ReplaceAll(authName,"\n","")
+	authName = strings.ReplaceAll(authName, "\n", "")
 
 	year := time.Now().Year()
 
 	tUUID := uuid.New()
 
-	tUUIDstr := strings.ToUpper(strings.ReplaceAll(tUUID.String(),"-",""))
+	tUUIDstr := strings.ToUpper(strings.ReplaceAll(tUUID.String(), "-", ""))
 
-	return FileFiller{AuthorName:authName,
-					  FileName:fName,
-					  FileNameCaps:fNameUpper,
-					  Year: year,
-					  UUID: tUUIDstr,
-					 }
+	return FileFiller{AuthorName: authName,
+		FileName:     fName,
+		FileNameCaps: fNameUpper,
+		Year:         year,
+		UUID:         tUUIDstr,
+	}
 }
 
 func main() {
 	const header = `/*
  * File:      {{.FileName}}.h
  * Author:    {{.AuthorName}}
- * 
- * Copyright: {{.Year}} {{.AuthorName}} 
+ *
+ * Copyright: {{.Year}} {{.AuthorName}}
  */
 #ifndef {{.FileNameCaps}}_H_{{.UUID}}
 #define {{.FileNameCaps}}_H_{{.UUID}} 1
@@ -85,26 +85,26 @@ func main() {
 	for _, arg := range args {
 		mFile := CreateFileFiller(arg)
 		// Check if header file exists, don't overwrite if it does
-		if _, err := os.Stat(mFile.FileName+".h"); err != nil {	
-			fmt.Println("Creating",mFile.FileName+".h")
-			headerFile, err := os.Create(mFile.FileName+".h")
+		if _, err := os.Stat(mFile.FileName + ".h"); err != nil {
+			fmt.Println("Creating", mFile.FileName+".h")
+			headerFile, err := os.Create(mFile.FileName + ".h")
 			if err != nil {
-				fmt.Println("ERROR:",err.Error())
+				fmt.Println("ERROR:", err.Error())
 				return
 			}
-			tHeader.Execute(headerFile,mFile)
+			tHeader.Execute(headerFile, mFile)
 			headerFile.Close()
 		}
-		
+
 		// Check if source file exists, don't overwrite if it does
-		if _, err := os.Stat(mFile.FileName+".cpp"); err != nil {
-			fmt.Println("Creating",mFile.FileName+".cpp")
-			cppFile, err := os.Create(mFile.FileName+".cpp")
+		if _, err := os.Stat(mFile.FileName + ".cpp"); err != nil {
+			fmt.Println("Creating", mFile.FileName+".cpp")
+			cppFile, err := os.Create(mFile.FileName + ".cpp")
 			if err != nil {
-				fmt.Println("ERROR:",err.Error())
+				fmt.Println("ERROR:", err.Error())
 				return
 			}
-			tBody.Execute(cppFile,mFile)
+			tBody.Execute(cppFile, mFile)
 			cppFile.Close()
 		}
 	}
